674b5469a77d37c2aeb6e20ed20d6f50a66ea2df
Kayleigh Whitehurst <kayleigh.whitehurst@outlook.com>
2021-12-01 09:27:53 +0000

    fixed up basic framework and tested working fine

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 
-class CodeGolf {
+class CodeGolf
+{
 
-    public function findPangrams($phrases)
+    public static function findPangrams($phrases)
     {
         //this can be done with Regexp but lets get it working first
-        
-        foreach ($phrases as $key => $phrase) {
-            
-            $checkingCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-            
-            foreach($checkingCharacters as $index => $checkingCharacter) {
-                if(strpos(strtolower($phrase),$checkingCharacter)) {
+
+        $correctPhrases = [];
+        foreach ($phrases as $phrase) {
+            $checkingCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+            foreach ($checkingCharacters as $index => $checkingCharacter) {
+                if (strpos(strtolower($phrase), $checkingCharacter) !== false) {
                     unset($checkingCharacters[$index]);
                 }
             }
-            if(count($checkingCharacters) == 0) {
+            if (count($checkingCharacters) == 0) {
                 $correctPhrases[] = $phrase;
             }
-
         }
-        
+
         return $correctPhrases;
     }
 }
 
 $phrases = [
     'I am coming in out of the cold for some soup',
-    'The quick brown fox jumped over the lazy dog',
+    'The quick brown fox jumps over the lazy dog', //correct
     'I am the walrus coo coo cachoo',
     'The Kids are not alright',
-    'Glib jocks quiz nymph to vex dwarf.'
-]
+    'Glib jocks quiz nymph to vex dwarf.', //correct
+];
 
-echo CodeGolf::findPangrams($phrases);
+var_dump(CodeGolf::findPangrams($phrases));
 
 ?>
