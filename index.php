2feac7d59094eddbb12d4bdb7ed6582833f851ac
Kayleigh Whitehurst <6371001+Kayrah87@users.noreply.github.com>
2021-12-01 08:58:06 +0000

    scaffolded the basic check in long-form

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+class CodeGolf {
+
+    public function findPangrams($phrases)
+    {
+        //this can be done with Regexp but lets get it working first
+        
+        foreach ($phrases as $key => $phrase) {
+            
+            $checkingCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+            
+            foreach($checkingCharacters as $index => $checkingCharacter) {
+                if(strpos(strtolower($phrase),$checkingCharacter)) {
+                    unset($checkingCharacters[$index]);
+                }
+            }
+            if(count($checkingCharacters) == 0) {
+                $correctPhrases[] = $phrase;
+            }
+
+        }
+        
+        return $correctPhrases;
+    }
+}
+
+$phrases = [
+    'I am coming in out of the cold for some soup',
+    'The quick brown fox jumped over the lazy dog',
+    'I am the walrus coo coo cachoo',
+    'The Kids are not alright',
+    'Glib jocks quiz nymph to vex dwarf.'
+]
+
+echo CodeGolf::findPangrams($phrases);
+
+?>
