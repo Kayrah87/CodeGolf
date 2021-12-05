6923077a0f32b0266b43c81eaea09bcd71494318
Kayleigh Whitehurst <kayleigh.whitehurst@outlook.com>
2021-12-05 17:28:37 +0000

    working solution

---
+++ Solution.php
@@ -0,0 +1,34 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param  Integer[]  $nums
+     *
+     * @return Integer
+     */
+    function removeDuplicates(&$nums)
+    {
+        return array_values(array_unique($nums));
+    }
+
+    function checkResult($solution, $expectedResult)
+    {
+        foreach($solution as $index => $integer) {
+            echo $integer . " = " . $expectedResult[$index];
+            if($integer !== $expectedResult[$index]) {
+                return "wrong";
+            }
+        }
+        return "Correct";
+    }
+}
+
+$solution = new Solution();
+$test = [0,0,1,1,1,2,2,3,3,4];
+$expectedResult = [0,1,2,3,4];
+$k = $solution->removeDuplicates($test);
+echo $solution->checkResult($k, $expectedResult);
+
+?>
