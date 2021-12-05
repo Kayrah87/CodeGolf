0610927e6f4174d4f702c431041d97ed0ef26772
Kayleigh Whitehurst <kayleigh.whitehurst@outlook.com>
2021-12-05 17:54:03 +0000

    added output count

--- Solution.php
+++ Solution.php
@@ -10,13 +10,12 @@ class Solution
      */
     function removeDuplicates(&$nums)
     {
-        return array_values(array_unique($nums));
+        return count(array_values(array_unique($nums)));
     }
 
     function checkResult($solution, $expectedResult)
     {
         foreach($solution as $index => $integer) {
-            echo $integer . " = " . $expectedResult[$index];
             if($integer !== $expectedResult[$index]) {
                 return "wrong";
             }
@@ -31,11 +30,6 @@ $test = [0,0,1,1,1,2,2,3,3,4];
 $expectedResult = [0,1,2,3,4];
 
 $k = $solution->removeDuplicates($test);
-echo $solution->checkResult($k, $expectedResult) . "\r\n";
-
-$test = [1,1,2];
-$expectedResult = [1,2];
-$k = $solution->removeDuplicates($test);
-echo $solution->checkResult($k, $expectedResult);
+var_dump($k);
 
 ?>
