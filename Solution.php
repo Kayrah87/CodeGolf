945a8aae8289e4d98da881e3521dc7b6f5014692
Kayleigh Whitehurst <kayleigh.whitehurst@outlook.com>
2021-12-05 17:34:26 +0000

    added second test for solution

--- Solution.php
+++ Solution.php
@@ -26,8 +26,15 @@ class Solution
 }
 
 $solution = new Solution();
+
 $test = [0,0,1,1,1,2,2,3,3,4];
 $expectedResult = [0,1,2,3,4];
+
+$k = $solution->removeDuplicates($test);
+echo $solution->checkResult($k, $expectedResult) . "\r\n";
+
+$test = [1,1,2];
+$expectedResult = [1,2];
 $k = $solution->removeDuplicates($test);
 echo $solution->checkResult($k, $expectedResult);
 
